5d02c12ff7dd027ebd838dd8c3ec87e06b4a517f
fatuma65 <namalefatuma28@gmail.com>
2024-04-02 18:27:43 +0300

    welcome the user to the page

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const Greet = () => {
-    return 'Hello world!';
+    alert('Welcome to this page');
+    document.getElementById('disappear').style.display = 'none'
+    document.getElementById('form').innerHTML = 'Welcome to github actions page';
+    console.log('Your name is being displayed')
+
 }
-console.log(Greet())
+// console.log(Greet())
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', Greet)
